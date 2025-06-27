4bb46d5edaa577a932816acf9a1b75ea19528d56
MohamedHesham2024 <mohmedhesham2024@gmail.com>
2025-06-27 16:03:40 +0300

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,ts}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#AF8255",
+        secondary: "#FFFFFF",
+        thread: "#F3F3F3",
+        fontPrimary: "#AF8255",
+        fontSecondary: "#7A7A7A",
+        fontThread: "#7A7A7A",
+        black: "#000000",
+        white: "#FFFFFF",
+        error: "#ef6b51",
+     },
+    },
+  },
+  plugins: [],
+};

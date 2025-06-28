c25406c525d68a69982867b7047015fd9242b67c
MohamedHesham2024 <mohmedhesham2024@gmail.com>
2025-06-29 01:54:20 +0300

    Feat: update routing to include About Us and Contact Us components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,14 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { FAQComponent } from './faq/faq.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
 
 export const routes: Routes = [{ path: '', component: HomeComponent},
   { path: 'faq', component: FAQComponent },
+  { path: 'about', component: AboutUsComponent },
+  { path: 'contact', component: ContactUsComponent },
+
+
 
  ];

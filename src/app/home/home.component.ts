da2d23d594fc693336b295267e758d8f6d8f7e9a
Amrkataria <amrkataria1234@gmail.com>
2025-06-30 11:38:38 +0300

    Feat: add analysis component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,10 +6,11 @@ import { LuxuryComponent } from "../luxury/luxury.component";
 import { DreamsComponent } from "../dreams/dreams.component";
 import { ClientsComponent } from "../clients/clients.component";
 import { DiscoverComponent } from "../discover/discover.component";
+import { AnalysisComponent } from "../sheard/analysis/analysis.component";
 
 @Component({
   selector: 'app-home',
-  imports: [NavbarComponent, HeroComponent, SliderComponent, LuxuryComponent, DreamsComponent, ClientsComponent, DiscoverComponent],
+  imports: [NavbarComponent, HeroComponent, SliderComponent, LuxuryComponent, DreamsComponent, ClientsComponent, DiscoverComponent, AnalysisComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })

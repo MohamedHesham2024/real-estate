d94ebc774fa3e73e41efe1144ed4c4eb6240fefc
Amrkataria <amrkataria1234@gmail.com>
2025-06-30 10:38:27 +0300

    Feat: handle scrollBar color and design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./navbar/navbar.component";
-
+import { WhatsAppComponent } from "./whats-app/whats-app.component";
+import { FooterComponent } from "./footer/footer.component";
+import { ThemeToggleService } from './theme-toggle.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent, WhatsAppComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'real-estate';
+   constructor(private themeService: ThemeToggleService) {
+  console.log('[AppComponent] ThemeToggleService injected');
+  }
 }

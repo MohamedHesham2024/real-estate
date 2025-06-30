5d34f36aa87949c32abf0ee327ac559cdac2dddf
Amrkataria <amrkataria1234@gmail.com>
2025-06-30 10:41:59 +0300

    Feat: create home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from "../navbar/navbar.component";
 import { HeroComponent } from "../hero/hero.component";
+import { SliderComponent } from "../slider/slider.component";
+import { LuxuryComponent } from "../luxury/luxury.component";
+import { DreamsComponent } from "../dreams/dreams.component";
+import { ClientsComponent } from "../clients/clients.component";
+import { DiscoverComponent } from "../discover/discover.component";
 
 @Component({
   selector: 'app-home',
-  imports: [NavbarComponent, HeroComponent],
+  imports: [NavbarComponent, HeroComponent, SliderComponent, LuxuryComponent, DreamsComponent, ClientsComponent, DiscoverComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })

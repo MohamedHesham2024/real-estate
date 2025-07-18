1babe41853789c6a88f83c0cf90314b158c85038
Amrkataria <amrkataria1234@gmail.com>
2025-07-18 19:47:20 +0300

    Feat: handle translate service

---
+++ src/app/language.service.ts
@@ -0,0 +1,36 @@
+
+import { Injectable } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LanguageService {
+
+ constructor(private translate: TranslateService) {
+    this.translate.addLangs(['en', 'ar', 'fr', 'pt']);
+    const savedLang = localStorage.getItem('lang') || 'en';
+    this.setLanguage(savedLang);
+  }
+
+ setLanguage(lang: string) {
+  localStorage.setItem('lang', lang);
+  this.translate.use(lang);
+
+  // تغيير الاتجاه في <html> tag
+  document.documentElement.dir = lang === 'ar' ? 'rtl' : 'ltr';
+  document.documentElement.lang = lang;
+
+  // تغيير الكلاس في <body> tag
+  document.body.classList.remove('rtl', 'ltr');
+  document.body.classList.add(lang === 'ar' ? 'rtl' : 'ltr');
+}
+
+  get currentLang() {
+    return this.translate.currentLang;
+  }
+
+  get availableLangs() {
+    return this.translate.getLangs();
+  }
+}

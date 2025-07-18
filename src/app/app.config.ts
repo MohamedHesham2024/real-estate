5985fd5049c787151d81c6ec15936f465ef35e8d
Amrkataria <amrkataria1234@gmail.com>
2025-07-18 19:47:02 +0300

    Feat: handle translate in app config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,18 +3,33 @@ import { provideRouter } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { ToastrModule } from 'ngx-toastr'
 import { routes } from './app.routes';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { provideHttpClient } from '@angular/common/http';
 
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http, './i18n/', '.json');
+}
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),
      provideAnimations(),
 
     // ✅ استيراد ToastrModule بخصائصه
     importProvidersFrom(
+         HttpClientModule,
       ToastrModule.forRoot({
         timeOut: 3000,
         positionClass: 'toast-top-right',
         preventDuplicates: true,
+      }),
+       TranslateModule.forRoot({
+        defaultLanguage: 'en',
+        loader: {
+          provide: TranslateLoader,
+          useFactory: HttpLoaderFactory,
+          deps: [HttpClient],
+        },
       })
     ),
      provideHttpClient()
